53002b1e655a9dafee1b3540f0aebecd4c27d670
Rômulo <107274618+rommuloifrn@users.noreply.github.com>
2024-12-13 16:49:42 +0000

    feat: initial page HTML & variables state

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+let nickName = $state('')
+
+</script>
+
+<h1>notlol</h1>
+<!-- https://svelte.dev/tutorial/svelte/text-inputs -->
+<form>
+    <input bind:value={nickName} placeholder="nick" />
+    <input type="text" name="" id="" placeholder="tagLine"/>
+    <button>Check</button>
+</form>
