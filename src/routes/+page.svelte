42f797ca48afaeeeb64ca481925ac1cc69dfcd0f
rommuloifrn <roomulo.cavalkant@outlook.com>
2024-12-13 22:57:14 -0300

    feat: add state for tagLine field

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,16 @@
 <script>
 let nickName = $state('')
+let tagLine = $state('')
 
 </script>
 
 <h1>notlol</h1>
+<ul>
+    <li>{nickName} / {tagLine}</li>
+</ul>
 <!-- https://svelte.dev/tutorial/svelte/text-inputs -->
 <form>
     <input bind:value={nickName} placeholder="nick" />
-    <input type="text" name="" id="" placeholder="tagLine"/>
+    <input bind:value={tagLine} type="text" name="" id="" placeholder="tagLine"/>
     <button>Check</button>
 </form>
