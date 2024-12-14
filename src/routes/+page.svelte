fbeb4db4afe17e031b5f7322b45f5bc913241f3d
rommuloifrn <roomulo.cavalkant@outlook.com>
2024-12-14 00:34:50 -0300

    feat: while-fetching message (flipping the "hidden" of a loading gif didnt work..)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,14 +10,30 @@
     async function getData() {
         console.log("start");
         
+        putOnMainElement("loading your request!")
+        //loadingIconSwitch()
+        
         let endpoint = `http://localhost:8080/main?gameName=${nickName}&tagLine=${tagLine}`;
         const response: any = await axios.get(endpoint);
-        const daysElement = document.getElementById('days');
+        
+        
+         putOnMainElement(
+            `${response.data} days without playing!`
+         );
 
-        daysElement!.innerHTML = `${response.data} days without playing!`;
         console.log("finish");
         return null;
     
+    function putOnMainElement(text:any) {
+        const daysElement = document.getElementById('days');
+        daysElement!.innerHTML = text;
+    }
+
+    function loadingIconSwitch() {
+        const loadingIcon = document.getElementById('loadingicon');
+        //loadingIcon!.hidden = !loadingIcon!.hidden;
+        loadingIcon!.hidden = !loadingIcon?.hidden
+    }
 }
 
 </script>
@@ -36,12 +52,13 @@
         </ul> -->
         <div id="days">
             search some player!
+            <img id="loadingicon" class="h-4 mx-auto" hidden src="loading.gif" alt="">
         </div>
         <!-- https://svelte.dev/tutorial/svelte/text-inputs -->
         <form onsubmit={()=>{getData}}>
             <input class="focus:outline-none bg-transparent" bind:value={nickName} placeholder="player name" />
             <span class="text-zinc-500">#</span><input class="focus:outline-none bg-transparent" bind:value={tagLine} type="text" name="" id="" placeholder="tagLine"/> <br>
-            <button class="font-semibold bg-red-500 text-white hover:bg-red-600 transition mt-5 w-full py-2 px-3 text-xl rounded-md" onclick={getData}>Check</button>
+            <button class="font-semibold bg-red-500 shadow-zinc-900 shadow-lg text-white hover:bg-red-600 transition mt-5 w-full py-2 px-3 text-xl rounded-md" onclick={getData}>Check</button>
         </form>
     </div>
     
